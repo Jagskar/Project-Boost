dbf02cf2b14139e9c022d5ea8802a29b13cd2852
Jagdeep <jagdeep.saredia@cadju.com>
2022-01-17 13:06:19 +0000

    Setting up prefabs, materials, scripts. Adding basic movement functions

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float thrustValue = 1000f;
+    [SerializeField]
+    float rotationSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            rb.AddRelativeForce(Vector3.up * thrustValue * Time.deltaTime);
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+            ApplyRotation(rotationSpeed);
+        else if (Input.GetKey(KeyCode.D))
+            ApplyRotation(-rotationSpeed);
+    }
+
+    void ApplyRotation(float rotationCurrFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationCurrFrame * Time.deltaTime);
+    }
+}
